36864aa6208184c928bfb60b64d155a7311d453c
Abdallah Alathamneh <abdallah@fthm.me>
2022-10-16 17:35:43 +0300

    adding APis

--- src/app/constants/api.ts
+++ src/app/constants/api.ts
@@ -5,7 +5,7 @@
 // Staging Environemnt
 
 // Local Dev Environment
-const ROOTLINK = 'http://127.0.0.1:8000';
+const ROOTLINK = 'http://127.0.0.1:8000/api';
 
 // 1- Students API
 export const STUDENT_API = ROOTLINK + '/students';
