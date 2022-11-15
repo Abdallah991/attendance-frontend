8c80121dc5ff740c5d0d2bbae6578c4bd4063b54
Abdallah Alathamneh <abdallah@fthm.me>
2022-11-15 17:12:05 +0300

    Navigation and rounded corners

--- src/app/constants/navigation.ts
+++ src/app/constants/navigation.ts
@@ -1,10 +1,24 @@
 export const navigationMenu = [
   {
     link: '/students',
-    iconPathInactive: '../../assets/icons/student.svg',
-    iconPathActive: '../../assets/icons/student.svg',
+    iconPathInactive: '../../assets/icons/overview.svg',
+    iconPathActive: '../../assets/icons/overview.svg',
+    active: false,
+    linkName: 'Overview',
+  },
+  {
+    link: '/users',
+    iconPathInactive: '../../assets/icons/students.svg',
+    iconPathActive: '../../assets/icons/students.svg',
     active: false,
     linkName: 'Students',
+  },
+  {
+    link: '/users',
+    iconPathInactive: '../../assets/icons/events.svg',
+    iconPathActive: '../../assets/icons/events.svg',
+    active: false,
+    linkName: 'Events',
   },
   {
     link: '/users',
