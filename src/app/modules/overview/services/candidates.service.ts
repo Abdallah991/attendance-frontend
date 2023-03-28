5a0c3c1325a4dddc9b71aa03fc1249738e893178
aalathm <abdulla.alathamnah@gmail.com>
2023-03-28 10:32:45 +0300

    candidates service

--- src/app/modules/overview/services/candidates.service.ts
+++ src/app/modules/overview/services/candidates.service.ts
@@ -3,46 +3,28 @@ import { Apollo, gql } from 'apollo-angular';
 import { Subscription } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
-
-public const GET_POSTS = gql`
-query GetPosts {
-  posts {
-    id
-    title
-  }
-}
-`
 export class CandidatesService {
-
-
   loading: boolean;
   posts: any;
 
-
- 
   private querySubscription: Subscription;
 
   constructor(private apollo: Apollo) {}
 
   public getUsers() {
-  // this.querySubscription = this.apollo
-  // .watchQuery<any>({
-  //   query: GET_POSTS
-  // })
-  // .valueChanges.subscribe(({ data, loading }) => {
-  //   this.loading = loading
-  //   this.posts = data.posts
-  // })
+    // this.querySubscription = this.apollo
+    // .watchQuery<any>({
+    //   query: GET_POSTS
+    // })
+    // .valueChanges.subscribe(({ data, loading }) => {
+    //   this.loading = loading
+    //   this.posts = data.posts
+    // })
+  }
+
+  ngOnDestroy() {
+    this.querySubscription.unsubscribe();
+  }
 }
-
-
-ngOnDestroy() {
-this.querySubscription.unsubscribe()
-}
-
-
-}
-
