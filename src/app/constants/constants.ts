aa45fcea1583e728dc7c0c67a0717ecf65f4cecd
aalathm <abdulla.alathamnah@gmail.com>
2023-04-03 10:57:54 +0300

    adding bio time token

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -23,3 +23,6 @@ export const PERMISSIN: SelectData[] = [
   { id: 'Admin', text: 'Admin' },
   { id: 'Operation', text: 'No' },
 ];
+
+export const bioTimeToken =
+  'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoyLCJ1c2VybmFtZSI6IkFiZGFsbGFoIiwiZXhwIjoxNjgxMTEzMzMzLCJlbWFpbCI6ImFiZGFsbGFoLmFsYXRoYW1uZWhAcmVib290MDEuY29tIiwib3JpZ19pYXQiOjE2ODA1MDg1MzN9.vdjmAdVkf9mKRpla1l-TL-rSTUjwjy2lfh18OM2v43o';
